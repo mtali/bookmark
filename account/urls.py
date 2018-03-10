b3fdf40ff1a11a354e897428063b47d2aba52ead
Mtali <emmanuelmtali@yahoo.com>
2018-03-11 00:33:39 +0300

    Update auth url to direct include all auth urls

--- account/urls.py
+++ account/urls.py
@@ -1,18 +1,10 @@
-from django.urls import path
-from django.contrib.auth.views import (
-    login, logout, logout_then_login,
-    password_change, password_change_done
-)
+from django.urls import path, include
 
 from . import views
 
 urlpatterns = [
     path('', views.dashboard, name="dashboard"),
-    path('login/', login, name='login'),
-    path('logout/', logout, name='logout'),
-    path('logout-then-login/', logout_then_login, name='logout_then_login'),
 
-    # change password urls
-    path('password-change/', password_change, name='password_change'),
-    path('password-change/done/', password_change_done, name='password_change_done'),
+    # include all athentication urls
+    path('', include('django.contrib.auth.urls')),
 ]
