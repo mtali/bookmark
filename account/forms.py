e1dbed757fc146b31725ff43552a8b12aef29d70
Mtali <emmanuelmtali@yahoo.com>
2018-03-11 17:07:14 +0300

    Code refactor

--- account/forms.py
+++ account/forms.py
@@ -2,9 +2,6 @@ from django import forms
 from django.contrib.auth.models import User
 
 from .models import Profile
-class LoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
 
 class UserRegistrationForm(forms.ModelForm):
     password = forms.CharField(label='Password', widget=forms.PasswordInput)
