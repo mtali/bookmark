610d3d3526706d8de67508c0897d973e58a5b0d4
Mtali <emmanuelmtali@yahoo.com>
2018-03-13 23:57:41 +0300

    Add detail page and configure statuc url in projects urls

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
 from . import views
 
@@ -16,6 +14,3 @@ urlpatterns = [
     # include all athentication urls
     path('', include('django.contrib.auth.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
