351a7073466d1fc0abf83e3e8baca5cd67b05b43
Mtali <emmanuelmtali@yahoo.com>
2018-03-14 19:56:13 +0300

    Add like functionality

--- images/views.py
+++ images/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
 
 from .forms import ImageCreateForm
 from .models import Image
@@ -29,5 +31,23 @@ def image_create(request):
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
+    users_likes = image.users_likes.all()
     return render(request, 'images/image/detail.html',
-            {'section': 'images', 'image': image})
+            {'section': 'images', 'image': image, 'users_likes': users_likes})
+
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == 'like':
+                image.users_likes.add(request.user)
+            else:
+                image.users_likes.remove(request.user)
+            return JsonResponse({'status': 'ok'})
+        except:
+            pass
+    return JsonResponse({'status': 'ko'})
