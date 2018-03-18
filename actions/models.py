e256deb06b02b487733118d5828813c174599322
Mtali <emmanuelmtali@yahoo.com>
2018-03-18 16:16:13 +0300

    Create Action model to relate user performing actions on other objects

--- actions/models.py
+++ actions/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
-# Create your models here.
+User = get_user_model()
+
+class Action(models.Model):
+    user = models.ForeignKey(User, related_name='actions', db_index=True, on_delete=models.CASCADE)
+    verb = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+    target_content_type = models.ForeignKey(
+        ContentType,
+        blank=True,
+        null=True,
+        related_name='target_object',
+        on_delete=models.CASCADE
+    )
+    target_id = models.PositiveIntegerField(
+        blank=True,
+        null=True,
+        db_index=True
+    )
+    target = GenericForeignKey('target_content_type', 'target_id')
+
+    class Meta:
+        ordering = ('-created_at',)
